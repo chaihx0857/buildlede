6bdbbed577b23f1993c80a7d620e71af5193dd78
chaihx0857 <66718751+chaihx0857@users.noreply.github.com>
2021-06-22 14:55:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,4 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
+git clone https://github.com/fangli/openwrt-vm-tools package/open-vm-tools
